0f38df24b1dea01d8381f743980e06257bbea4f9
Elson-KaiDong <elsonchongkaidong@gmail.com>
2023-04-23 20:24:21 +0800

    ASP.net API

---
+++ Models/Todo.cs
@@ -0,0 +1,14 @@
+﻿namespace TodoAPI.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+
+
+    }
+
+   
+}
